7a98b1f4d14e5e36d6e7c38538a4978e3bca5820
Marcos <marcosantoniozovico@hotmail.com>
2019-12-28 18:58:22 -0300

    A Sample Development Environment (PHP/Apache/MySQL) using docker-compose.yml

---
+++ docker-compose/step-4/index.php
@@ -0,0 +1,19 @@
+<?php
+header("content-type: text");
+$host = "db";
+$username = "root";
+$pw = "my!!!root!!!pw";
+
+
+$conn = new mysqli($host,$username,$pw);
+
+if ($conn -> connect_errno > 0){
+    echo $db -> connect_error;
+} else {
+    echo "DB Connection successfull\n\n";
+
+    $result = mysqli_query($conn, "SHOW DATABASES;");
+    while( $row = mysqli_fetch_row($result)){
+        echo $row[0]."\n";
+    }
+}
